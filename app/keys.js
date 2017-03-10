2da25cdd49f63ed8e4a6c8c7524f6664f0bdd841
Claire Freehafer <claire.freehafer@gmail.com>
2017-03-10 10:26:22 -0500

    data extracted

---
+++ app/keys.js
@@ -0,0 +1,81 @@
+const request = require('request-promise');
+
+const locationKeysUrl = 'http://www.cleardarksky.com/t/chart_prop00.txt';
+
+const keysArr = [];
+const locationArr = [];
+const nameArr = [];
+
+let locationKeysArr;
+let locationData = {};
+
+const	getLocationArray = function () {
+	return request('http://pastebin.com/raw/FzGVtrtc')
+	.then(function (body) {
+		locationKeysArr = body.split(`\n`); // 'key|location|name'
+
+		locationKeysArr.forEach(function (element) {
+			keysArr.push(element.split('|')[0]);
+			locationArr.push(element.split('|')[1]);
+			nameArr.push(element.split('|')[2]);
+		})
+	})
+	.catch(console.error)
+}
+
+const getLocationData = function (key) {
+	const locationDataUrl = `http://cleardarksky.com/txtc/${key}csp.txt`;
+	locationData.key = key;
+	locationData.conditions = [];
+	locationData.darkness = [];
+
+	return request('http://pastebin.com/raw/jegKE25p')
+	.then(function (body) {
+		locationData.name = /title(\s*)=(\s*)\"(.*)\"/g.exec(body)[3];
+		let dataArr = body.match(/"(.*)",\s(\d*),\t(\d*),\t(\d*),\t(\d*),\t(\d*),\t(\d*),\t/g); // array of each hour's data
+		let darknessArr = body.match(/"(.*)",\s([-\.\d]*),\s([-\.\d]*),\s([-\.\d]*)(\))/g);
+		// console.log(dataArr)
+
+		dataArr.forEach(function (data) {
+			let hourlyData = data.replace(/\s/g, '').split(','); // one array with each column its own element
+			let hour = hourlyData[0].split(':')[0] // YYYY-MM-DDHH
+
+			locationData.conditions.push({
+				time: [hour.substr(hour.length - 2, hour.length), 00, 00], // [HH, MM, SS]
+				clouds: hourlyData[1],
+				transparency: hourlyData[2],
+				seeing: hourlyData[3],
+				wind: hourlyData[4],
+				humidity: hourlyData[5],
+				temperature: hourlyData[6]
+			})
+		})
+
+		darknessArr.forEach(function (data, index) {
+			let darknessData = data.replace(/\s|\)/g, '').split(',');
+			let time = darknessData[0].slice(11, -1).split(':');
+			// console.log(time)
+
+			locationData.darkness.push({
+				time: [time[0], time[1], time[2]], // [HH, MM, SS];
+				limitingMagnitude: darknessData[1],
+				sunAltitude: darknessData[2],
+				moonAltitude: darknessData[3]
+			})
+		})
+		return locationData;
+	})
+	.catch(console.error);
+}
+
+
+module.exports = {
+	getLocationArray,
+	keysArr,
+	locationArr,
+	nameArr,
+	getLocationData
+}
+
+
+
---
+++ index.js
@@ -0,0 +1,6 @@
+const keys = require('./app/keys');
+
+
+
+// keys.getLocationArray().then(res => console.log(keys.keysArr))
+keys.getLocationData()
